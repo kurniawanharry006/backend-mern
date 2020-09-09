a0f452f875717690ef23ec4bd558d98fa6d497dc
kurniawanharry006 <53112352+kurniawanharry006@users.noreply.github.com>
2020-09-09 18:56:54 +0700

    api-landing-page

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,6 +1,7 @@
 const Item = require('../models/Item')
 const Treasure = require('../models/Activity')
 const Traveler = require('../models/Booking')
+const Category = require('../models/Category')
 
 
 module.exports = {
@@ -11,9 +12,36 @@ module.exports = {
             .limit(5)
             .populate({path:'imageId', select:'_id imageUrl'})
 
+            const category = await Category.find()
+            .select('_id name')
+            .limit(3)
+            .populate({
+                path:'itemId', 
+                select:'_id title country city isPopular imageUrl',
+                perDocumentLimit:4,
+                option:{sort:{sumBooking:-1}},
+                populate:{
+                    path:'imageId', 
+                    select:'_id imageUrl',
+                    perDocumentLimit: 1
+                }
+            })
+
             const traveler = await Traveler.find();
             const treasure = await Treasure.find()
             const city = await Item.find()
+
+            for(let i=0; i<category.length; i++){
+                for(let j=0; j<category[i].itemId.length; j++){
+                    const item = await Item.findOne({_id:category[i].itemId[j]._id})
+                    item.isPopular = false
+                    await item.save()
+                        if(category[i].itemId[0] === category[i].itemId[j]){
+                            item.isPopular = true
+                            await item.save()
+                        }
+                }
+            }
 
             res.status(200).json({
                 hero:{
@@ -21,11 +49,13 @@ module.exports = {
                     treasure:treasure.length,
                     cities:city.length 
                 },
-                mostPicked
+                mostPicked,
+                category
             })
 
         } catch (error) {
-            
+            console.log(error);
+            res.status(500).json({message:'Internal server error'})
         }
     }
 }
